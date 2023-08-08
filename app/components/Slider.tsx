abd25cc1930b404717e5b33eeb732335864638d1
Kiran <kiranpadma2003@gmail.com>
2023-08-08 22:57:16 +0530

    hero slider completed

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -1,18 +1,53 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { preinit } from "react-dom";
+
+const data = [
+  {
+    id: 1,
+    title: "always fresh & always crispy & always hot",
+    image: "/slide1.png",
+  },
+  {
+    id: 2,
+    title: "we deliver your order wherever you are in NY",
+    image: "/slide2.png",
+  },
+  {
+    id: 3,
+    title: "the best pizza to share with your family",
+    image: "/slide3.jpg",
+  },
+];
 
 const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(2);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () =>
+        setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
+      4000
+    );
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-8rem)]">
+    <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-8rem)] lg:flex-row bg-fuchsia-50">
       {/* text container */}
-      <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
-        <h1 className="text-5xl uppercase text-center p-4 md:text-5xl xl:text-6xl">Test</h1>
-        <button className="bg-red-500 text-white py-4 px-8 rounded-md">Order Now</button>
+      <div className=" flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
+        <h1 className="text-5xl uppercase text-center p-4  sm:px-16 md:px-10 md:text-5xl xl:text-6xl">
+          {data[currentSlide].title}
+        </h1>
+        <button className="bg-red-500 text-white py-4 px-8 rounded-md">
+          Order Now
+        </button>
       </div>
       {/* image container */}
-      <div className="h-1/2 w-full relative ">
+      <div className=" w-full relative flex-1 ">
         <Image
-          src="/slide1.png"
+          src={data[currentSlide].image}
           alt="Cover pic"
           fill
           className="object-cover"
